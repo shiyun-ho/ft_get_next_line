1f599f24ab69498d88bb60d3b302ee7c8c82a24d
Shiyun <serene.hoshiyun@gmail.com>
2024-08-24 21:23:25 +0800

    Printing all except first char

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 17:22:23 by hshi-yun          #+#    #+#             */
-/*   Updated: 2024/08/21 17:48:38 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2024/08/24 20:17:17 by hshi-yun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,8 @@
 
 char    *get_next_line(int fd);
 void    *ft_calloc(size_t n, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2, size_t size);
+int		ft_strchr_index(const char *s, int c);
 
 #endif
