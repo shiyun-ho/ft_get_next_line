8dcbaa637ea68ef15624ce64b4f41752a2613a23
Shiyun <serene.hoshiyun@gmail.com>
2024-09-01 13:24:14 +0800

    Cleaned up some code

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hshi-yun <hshi-yun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shiyun <shiyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 17:04:41 by hshi-yun          #+#    #+#             */
-/*   Updated: 2024/08/31 22:30:52 by hshi-yun         ###   ########.fr       */
+/*   Updated: 2024/09/01 00:53:42 by shiyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,34 @@ int    process_stash(char **stash, char **line)
     }
     return (0);
 }
+
+char    *process_buffer(ssize_t bytes, char **buffer, char **stash, char **line)
+{
+    if (bytes < 0)
+        // return (free_resources(&buffer, &stash, NULL), NULL);
+        return (free_resources(buffer, stash, NULL), NULL);
+    if (bytes == 0)
+    {
+        if (!line)
+        {
+            if (stash)
+            {
+                *line = (char *)ft_calloc(1, sizeof(char));
+                *line = ft_strjoin(*line, *stash, 0, ft_strlen(*stash) - 1);
+                free_resources(stash, buffer, NULL);
+                if (!line || !line[0])
+                    return (free(line), line = NULL, NULL);
+                return (*line);
+            }
+            free_resources(NULL, buffer, NULL);
+            return (*line);   
+        }
+        else
+            return (NULL);
+    }
+    *buffer[bytes] = '\0';
+    return (*buffer);
+}
 /**
  * @brief: Function that returns a line read from a file descriptor
  * @param: fd: The file descriptor to read from
@@ -75,29 +103,32 @@ char    *get_next_line(int fd)
         
         buffer = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
         bytes_read = read(fd, buffer, BUFFER_SIZE);
+
+        // if (bytes_read < 0)
+        //     return (free_resources(&buffer, &stash, NULL), NULL);
+        // if (bytes_read == 0)
+        // {
+        //     if (!line)
+        //     {
+        //         if (stash)
+        //         {
+        //             line = (char *)ft_calloc(1, sizeof(char));
+        //             line = ft_strjoin(line, stash, 0, ft_strlen(stash) - 1);
+        //             free_resources(&stash, &buffer, NULL);
+        //             if (!line || !line[0])
+        //                 return (free(line), line = NULL, NULL);
+        //             break;
+        //         }
+        //         return (free(buffer), buffer = NULL, line);   
+        //     }
+        //     else
+        //         return (NULL);
+        // }
+        // buffer[bytes_read] = '\0';
+        //TODO: Resolve!
+        line = process_buffer(bytes_read, &buffer, &stash, &line);
         
-        if (bytes_read < 0)
-            return (free_resources(&buffer, &stash, NULL), NULL);
-        if (bytes_read == 0)
-        {
-            if (!line)
-            {
-                if (stash)
-                {
-                    line = (char *)ft_calloc(1, sizeof(char));
-                    line = ft_strjoin(line, stash, 0, ft_strlen(stash) - 1);
-                    free_resources(&stash, &buffer, NULL);
-                    if (!line || !line[0])
-                        return (free(line), line = NULL, NULL);
-                    break;
-                }
-                return (free(buffer), buffer = NULL, line);   
-            }
-            else
-                return (NULL);
-        }
-        buffer[bytes_read] = '\0';
-        
+        //process_to_line(char **buffer, char **stash, char **line)
         int buffer_newline_index = ft_strchr_index(buffer, '\n');
         if (!stash)
             stash = (char *)ft_calloc(1, sizeof(char));
@@ -128,3 +159,4 @@ char    *get_next_line(int fd)
     }
     return (line);
 }
+
